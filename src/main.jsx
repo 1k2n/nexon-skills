cf354681ca49fa08e467fa04c7b5063401f03dbe
ikenn <8907396+1k2n@users.noreply.github.com>
2026-02-07 18:50:19 +0900

    + Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'https://esm.sh/react@18.2.0?dev';
 import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client?dev';
-import { App } from 'app.jsx';
+import { App } from './app.jsx';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
